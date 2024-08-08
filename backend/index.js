61475972368abaf3596fa4d318ac835e985e865a
ZainHanif29 <zainhanif2902@gmail.com>
2024-08-08 16:56:19 +0500

    server running

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = express();
+const PORT = 8000;
+
+app.listen(PORT,()=>{
+    console.log('server is running',PORT);
+})
