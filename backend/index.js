ca3ec51d7c0550e248a9b44238f700210c90de9d
ZainHanif29 <zainhanif2902@gmail.com>
2024-08-08 17:34:45 +0500

    db work

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,32 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config();
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT;
 
-app.listen(PORT,()=>{
-    console.log('server is running',PORT);
-})
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Server is running",
+    success: true,
+  });
+});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("server is running", PORT);
+});
---
+++ backend/utils/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URl}/${process.env.MONGO_DB}`);
+    console.log("Mongodb connected successfully");
+  } catch (error) {
+    console.log(`Mongodb connected unsuccessfully ${error}`);
+  }
+};
+
+export default connectDB;
